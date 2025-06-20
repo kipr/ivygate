1ea7478efe2ef89bd3dccd327051d9187d9da08d
Erin <erinharrington12@yahoo.com>
2025-06-20 11:13:37 -0500

    Export components for IDE

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,5 @@ export * from './Ivygate';
 export * from './IvygateFileExplorer'
 export * from './Message';
 export * from './MotorServoSensorDisplay';
+export { default as DocumentationWindow } from './components/documentation/DocumentationWindow';
+export * from './FileUploader';
