cb3189c8d7673b9800b97576890c5e329b7e5103
Erin <erinharrington12@yahoo.com>
2025-07-17 10:21:25 -0500

    Project and file creation types

---
+++ src/types/creationTypes.ts
@@ -0,0 +1,17 @@
+export enum ProjectCreationType {
+  ADD = '+',
+  CREATE_PROJECT = 'Create Project',
+  UPLOAD_PROJECT = 'Upload Project',
+}
+
+export enum FileCreationTypeAction {
+  ADD = '+',
+  CREATE_FILE = 'Create File',
+  UPLOAD_FILE = 'Upload File',
+}
+
+export enum FileCreationTypeActionSimple {
+  ADD = '+',
+  CREATE_FILE = 'Create File',
+
+}
