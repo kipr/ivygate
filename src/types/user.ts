d65a4d14952fdcdee2e2609b12bdca15fff41900
Erin <erinharrington12@yahoo.com>
2025-08-12 10:54:40 -0500

    Added UploadUser type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,12 +1,20 @@
-import { Project } from './project';
+import { Project, UploadedProject } from './project';
 import { InterfaceMode } from './interface';
 import Classroom  from './classroomTypes';
-
+import { FileInfo } from './fileInfo';
 export type User = {
   userName: string;
   interfaceMode: InterfaceMode;
   projects: Project[];
   classroomName?: string;
+};
+
+export type UploadedUser = {
+   configFile: FileInfo;
+  userName: string;
+  interfaceMode: InterfaceMode;
+  projects: UploadedProject[];
+   classroomName?: string; 
 };
 
 export const BLANK_USER: User = {userName: '', interfaceMode: InterfaceMode.SIMPLE, projects: [], classroomName: ''};
